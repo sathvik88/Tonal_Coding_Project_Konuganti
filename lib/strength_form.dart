7fc448ef28c0573c11600eb3284696e9f7205118
Sathvik Konuganti <sathvikkonuganti@Sathviks-MacBook-Pro.local>
2022-06-28 16:31:48 -0400

    Adding finaly changes

--- lib/strength_form.dart
+++ lib/strength_form.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:tonal_troject_konuganti/styles.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class StrengthForm extends StatefulWidget {
   @override
@@ -12,79 +13,64 @@ class StrengthForm extends StatefulWidget {
 }
 
 class StrengthFormState extends State<StrengthForm>{
-  late int upperInt = 0;
-  late int coreInt;
-  late int lowerInt;
-  late String upper;
-  late String core;
-  late String lower;
-
+  final  _weightController = TextEditingController();
+  final _labelController = TextEditingController();
+  String _label="";
+  String _weight="";
 
   final _formKey = GlobalKey<FormState>();
-  
-  Widget _buildCoreField(){
-    return Column(
-      children: [
-        TextFormField(
-          key: _formKey,
-          keyboardType: TextInputType.numberWithOptions(),
-          decoration: InputDecoration(
-              labelText: "Upper Weight"
-          ),
-          validator: (value){
-            if(value!.isEmpty){
-              return "Weight is required";
-            }
-            upperInt = int.parse(value);
-            if(upperInt < 0 || upperInt > 350){ //weight constraints
-              return "Please enter a weight between 0-350";
-            }
-          },
+  final _labelKey = GlobalKey<FormState>();
 
-        ),
-        Container(
-          child: ElevatedButton(
-            onPressed: (){
-              if(_formKey.currentState!.validate()){
-                return;
-              }
-            },
-            child: Text("Button"),
-          ),
-        )
-      ],
-    );
+  void onTap(){
+    if(_formKey.currentState!.validate()){
+      setState((){
+        _weight = _weightController.text;
+      });
+    }else{
+      return null;
+    }
+    if(_labelKey.currentState!.validate()){
+      setState((){
+        _label = _labelController.text;
+      });
+    }else{
+      return null;
+    }
+
   }
-  
+
+
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+        resizeToAvoidBottomInset: false,
       appBar: AppBar(title: Text("Strength Form")),
       body: Container(
         child: Stack(
           children: [
             Container(
-              width: 530,
-              height: 320,
+              width: 550,
+              height: 350,
               decoration: bubbleBoxDecoration,
             ),
             Container(
               padding: EdgeInsets.all(50),
               alignment: Alignment.topCenter,
               child: Text(
-                "Upper Body",
+                _label,
                 textAlign: TextAlign.center,
                 style: labelTextStyle,
               ),
             ),
             Container(
-              padding: EdgeInsets.all(80),
+              padding: EdgeInsets.fromLTRB(80, 70, 80, 400),
               alignment: Alignment.topCenter,
-              child: Text(
-                "${upperInt}",
-                textAlign: TextAlign.center,
+              child: AutoSizeText(
+                _weight,
                 style: weightTextStyle,
+
               ),
             ),
             Container(
@@ -106,17 +92,64 @@ class StrengthFormState extends State<StrengthForm>{
                 FocusManager.instance.primaryFocus?.unfocus();
               },
               child: SingleChildScrollView(
-                  child: Container(
-                    padding: EdgeInsets.only(top:390),
-                    child: _buildCoreField(),
+
+                child: Container(
+                  padding: EdgeInsets.only(top:390),
+                  child: Form(
+                    key: _formKey,
+                    child: TextFormField(
+                    controller: _weightController,
+                    keyboardType: TextInputType.numberWithOptions(),
+                    decoration: InputDecoration(labelText: 'Weight'),
+                    validator: (val){
+                      if(val!.isEmpty){
+                        return "Please enter a value";
+                      }
+                      int weight = int.parse(val); //Converting string to int for weight range.
+                      if(weight < 0 || weight > 350){ //Making sure input is within the weight range.
+                        return "Please enter a value between 0-350lbs";
+                      }
+                    },
+                  ),
                   ),
 
-              )
+                ),
+
+              ),
             ),
+            GestureDetector(
+              onTap: (){
+                FocusManager.instance.primaryFocus?.unfocus();
+              },
+              child: Container(
+                padding: EdgeInsets.only(top:450),
+                child: Form(
+                  key: _labelKey,
+                  child: TextFormField(
+                    controller: _labelController,
+                    decoration: InputDecoration(labelText: 'Strength Label'),
+                    validator: (val){
+                      if(val!.isEmpty){
+                        return "Please enter a label name";
+                      }
+
+                    },
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+                padding: EdgeInsets.only(top:500),
+              child: ElevatedButton(
+                onPressed: onTap,
+                child: Text("Button")
+              ),
+
+            )
 
           ],
         ),
-
 
       ),
     );
@@ -125,9 +158,8 @@ class StrengthFormState extends State<StrengthForm>{
 
 }
 
-mixin InputValidationMixin{
-  bool isWeigthValid(String weight){
 
-  }
-}
+
+
+
 
